e9c68b1b790db8064279fb2739e82b1207d0e7ba
alexsoftqa <46721987+alexsoftqa@users.noreply.github.com>
2020-03-24 08:31:39 -0400

    add wrapper

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -3,7 +3,7 @@ public class MyFirstProgram {
 
     public static void main (String[] args) {
 
-        System.out.println("Hello, fucking World!!!");
+        System.out.println("Hello, awesome  World!!!");
     }
 }
 
