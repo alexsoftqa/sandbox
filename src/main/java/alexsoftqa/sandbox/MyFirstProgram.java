2e84274ca2276afc08d4ac860c631663b2e14d14
alexsoftqa <46721987+alexsoftqa@users.noreply.github.com>
2020-03-24 16:58:23 -0400

    hghg

--- src/main/java/alexsoftqa/sandbox/MyFirstProgram.java
+++ src/main/java/alexsoftqa/sandbox/MyFirstProgram.java
@@ -1,10 +1,26 @@
 package alexsoftqa.sandbox;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MyFirstProgram {
 
     public static void main (String[] args) {
+        hello("World");
+        hello("User");
+        hello("Alex");
 
-        System.out.println("Hello, Awesome World!!!");
+       double l = 5;
+        System.out.println("Square area with side " + l + " = " + area(l));
     }
+    public static void hello(String somebody) {
+        System.out.println("Hello, Crazy " + somebody + "!!!");
+    }
+    public static double area (double l) {
+        return  l * l;
+
+
+    }
+
+
 }
 
